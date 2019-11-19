66cc8c8b20fcbad76cc98066d9daa7b92bafb9d5
jonach1998@ <jonach1998@gmail.com>
2019-11-18 18:38:58 -0600

    Initial commit

---
+++ numeros_diferentes_2_listas.py
@@ -0,0 +1,29 @@
+# A la antigua
+a = [1, 10, 5, 3, 8]
+b = [8, 80, 100, 3, 10]
+c = list()
+notrepeted = 0
+for item1 in a:
+    for item2 in b:
+        if item1 != item2:
+            notrepeted = 1
+        else:
+            notrepeted = 0
+            break
+    if notrepeted:
+        c.append(item1)
+
+for item1 in b:
+    for item2 in a:
+        if item1 != item2:
+            notrepeted = 1
+        else:
+            notrepeted = 0
+            break
+    if notrepeted:
+        c.append(item1)
+print(c)
+
+# Forma optimizada
+c = [item for item in a + b if item not in b or item not in a]
+print(c)
